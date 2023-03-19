3e9bcbd724885b23547683ebb99ae5ebc006a99a
Nikita Kovalev <nikita@maizy.dev>
2023-03-19 20:03:30 +0500

    iss #1: project template

---
+++ src/main/java/dev/maizy/myna/MynaApplication.java
@@ -0,0 +1,13 @@
+package dev.maizy.myna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynaApplication.class, args);
+	}
+
+}
