970999cd7be0d453af40b9efe7e893e581e74fd8
Nikita Kovalev <nikita@maizy.dev>
2024-01-06 22:19:25 +0100

    iss #4: migrations for games & game players

--- src/main/resources/db/changelog/changelog.sql
+++ src/main/resources/db/changelog/changelog.sql
@@ -10,3 +10,31 @@ create table rulesets (
 --changeset myna:2
 create index rulesets_name_idx on rulesets ((ruleset ->> 'name'), id);
 --rollback drop index rulesets_name_idx
+
+--changeset myna:3
+create type game_state as enum ('created', 'upcomming', 'launched', 'finished');
+--rollback drop type game_state
+
+--changeset myna:4
+create table games(
+  id text not null primary key,
+  ruleset_id text not null references rulesets(id),
+  state game_state not null default 'created',
+  created_at timestamp not null,
+  finished_at timestamp,
+  owner_uid text not null
+);
+--rollback drop table games
+
+--changeset myna:5
+create table game_players(
+  game_id text not null references games(id),
+  ruleset_player_id text not null,
+  name text not null,
+  uid text,
+  join_key text,
+  joined_at timestamp,
+  ruleset_order int not null default 0,
+  primary key (game_id, ruleset_player_id)
+);
+--rollback drop table game_players
